03a9ff00119011b45126ada8e58be8ebf5a34111
Codica Studio <CodicaStudioDev@gmail.com>
2020-08-20 10:49:29 -0500

    Initial commit.

---
+++ src/Facades/PdfReportFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace codicastudio\ReportGenerator\Facades;
+
+use Illuminate\Support\Facades\Facade as IlluminateFacade;
+
+/**
+ * @see \codicastudio\ReportGenerator\ReportMedia\PdfReport
+ */
+class PdfReportFacade extends IlluminateFacade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'pdf.report.generator';
+    }
+}
